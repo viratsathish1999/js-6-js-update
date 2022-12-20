db2a5fd9006fe684eb33dd9b3113e1ff10a63cc5
viratsathish1999 <sathish.r9688@gmail.com>
2022-12-20 16:56:06 +0530

    add file

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+var a=parseInt(prompt("main array length"));
+var b=[];
+for(i=0 ;i<a;i++){
+	b[i]=parseInt(prompt("Enter array value"));
+}
+document.write(b); 
+
+var c;
+c=parseInt(prompt("main array index value"));
+b[c]=parseInt(prompt("main array new value"));
+document.write("<br>"+  b); 
+
+
+
+
+
+  
